fa13a6787577bd2b1477a0a5df22cb587fd3249a
zoelhapner <achmadzulkiflinurrochim@gmail.com>
2026-08-13 15:44:54 +0700

    coba ganti migrasi tabel

--- database/migrations/migrasi/2026_08_13_145331_create_zhpicture_finance_tables.php
+++ database/migrations/migrasi/2026_08_13_145331_create_zhpicture_finance_tables.php
@@ -13,26 +13,36 @@ return new class extends Migration
         DB::statement('CREATE SCHEMA IF NOT EXISTS zhpicture');
 
         /*
-         * Clone struktur tabel Finance dari public
-         *
-         * public = Finance LebihterSystem
-         * zhpicture = Finance ZH Picture
+         * Urutan penting karena ada Foreign Key.
+         * Parent dibuat dan diisi terlebih dahulu.
          */
-
         $tables = [
             'accounting_accounts',
-            'accounting_closing_balances',
+            'accounting_periods',
+            'accounting_journals',
             'accounting_journal_details',
             'accounting_journal_enclosures',
-            'accounting_journals',
-            'accounting_periods',
+            'accounting_closing_balances',
             'opening_balances',
         ];
 
         foreach ($tables as $table) {
+
+            /*
+             * 1. Copy struktur
+             */
             DB::statement("
                 CREATE TABLE IF NOT EXISTS zhpicture.{$table}
                 (LIKE public.{$table} INCLUDING ALL)
+            ");
+
+            /*
+             * 2. Copy seluruh data
+             */
+            DB::statement("
+                INSERT INTO zhpicture.{$table}
+                SELECT *
+                FROM public.{$table}
             ");
         }
     }
